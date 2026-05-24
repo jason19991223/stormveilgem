6aaca541952c06fe32a7069453e958eb25efa707
茶茶荼荼 <66516339+jason19991223@users.noreply.github.com>
2026-05-24 20:36:50 +0800

    Initial backup of Stormveil Gem site

---
+++ stormveilgem-github-upload/lib/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "嵐晶之聲",
+  url: process.env.NEXT_PUBLIC_SITE_URL || "https://stormveilgem.com",
+  description: "礦石、天然石飾品、手作設計與日常穿搭靈感。",
+  email: "jason881223@gmail.com",
+  instagramHandle: "stormveil_gem",
+  instagramUrl: "https://www.instagram.com/stormveil_gem/",
+  shopeeUrl:
+    "https://shopee.tw/miniputin?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQPOTM2NjE5NzQzMzkyNDU5AAGnDOYHVm3zItuugh4tCBMwXlE6QOtshXbOCcftcFgSc1QtTctQN8x-Muq2vTc_aem_T2umqpwf8s-bJMfGYrBcmQ"
+};
